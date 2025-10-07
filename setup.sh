2cbac6c776c54abcf404d59964211a02fe0ed858
itzAkss <akshahanaisultan97@gmail.com>
2025-10-07 14:20:03 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-pkg update && pkg upgrade -y && pkg install python python3 python-pip && pip install  pillow && pip install qrcode[pil]
+pkg update && pkg upgrade -y && pkg install python python3 python-pip && pip install libjpeg-turbo libpng freetype libtiff littlecms pillow qrcode[pil]
